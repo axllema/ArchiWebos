27e584c55e3dff5b05b2e796d7e791088f588291
Axelle Maolé <axellexm@gmail.com>
2023-10-09 18:48:25 +0200

    creation of gallery and adding images in index js, for the portfolio

--- frontend/assets/JS/index.js
+++ frontend/assets/JS/index.js
@@ -5,29 +5,44 @@ let categories = ("http://localhost:5678/api/categories");
 const gallery = document.getElementById('gallery')
 const portfolio = document.querySelector('#portfolio');
 
-async function getWorks() {
-    const response = await fetch(works) 
-    const responseWork = await response.json()
-
-for (let i = 0 ; i < responseWork.length; i++){
-console.log(responseWork [i])
-}}
-
-getWorks()
-
-async function getCategories() {
-    const response = await fetch(categories) 
-    const responseCategorie = await response.json()
-
-for (let i = 0 ; i < responseCategorie.length; i++){
-console.log(responseCategorie [i])
-}}
-
-getCategories()
-
-
-/* for (let i = 0; i < ######.length; i++) {
-const imageElement = document.createElement("img");
-imageElement.src = idk.image;
-#####.appendChild(###); */
+function createGallery(works) {
+    works.forEach((work) => {
+      // on crée les éléments HTML
+      const figure = document.createElement('figure');
+      const image = document.createElement('img');
+      const figcaption = document.createElement('figcaption');
+  
+      // on ajoute les données aux éléments HTML
+      image.src = work.imageUrl;
+      image.alt = work.title;
+      figcaption.textContent = work.title;
+  
+      // on ajoute l'img et le figcaption au figure
+      figure.appendChild(image);
+      figure.appendChild(figcaption);
+  
+      // on ajoute les éléments HTML à la gallery
+      gallery.appendChild(figure);
+    });
+  }
+  
+  async function getWorks() {
+    const response = await fetch(works);
+    const responseWork = await response.json();
+    createGallery(responseWork);
+  }
+  
+  getWorks();
+  
+  async function getCategories() {
+    const response = await fetch(categories);
+    const responseCategorie = await response.json();
+  
+    for (let i = 0; i < responseCategorie.length; i++) {
+      console.log(responseCategorie[i]);
+    }
+  }
+  
+  getCategories();
+  
 

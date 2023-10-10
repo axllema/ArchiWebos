f654c1a8a53d0c951888c90687536df616486418
Axelle Maolé <axellexm@gmail.com>
2023-10-10 17:15:24 +0200

    trying login js, errors

--- frontend/assets/JS/login.js
+++ frontend/assets/JS/login.js
@@ -1,24 +1,34 @@
 const loginForm = document.getElementById('login_form');
+const submitButton = document.getElementById('submit_button');
 
-/* 
-loginForm.addEventListener("submit", function (event) {
-*/
+loginForm.addEventListener('submit', async function (event) {
 
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
+  //prevent refreshing the whole page every time the button is clicked
+  event.preventDefault();
 
-const user = {
-  email: email,
-  password: password,
-};
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const user = {
+    email: email.value,
+    password: password.value
+  };
 
-console.log(user)
-
-fetch("http://localhost:5678/api/users/login");
-
-/*
-curl -X 'POST' \
-  'http://localhost:5678/api/users/login' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  */
+await fetch('http://localhost:5678/api/users/login', {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  /* assigning an object with keys that represent the information the server needs
+  JSON.stringify() means we are sending the information in a string form to the server */
+  body: JSON.stringify(user),
+  })
+  .then((response) => response.json())
+  .then((result) => {
+    if(result.message === “SUCCESS”){
+      alert(“You are logged in.”);
+      this.goToMain(index.html);
+     } else {
+         alert('Erreur lors de la connexion');
+     }
+  });
+})

5d5cd0a058fce9acc04a84996129baae823b6de6
axllema <axellexm@gmail.com>
2023-10-18 17:06:44 +0200

    first try at modal

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -69,18 +69,16 @@ function updateLoginLink() {
     }
 }
 
+updateLoginLink();
+
 function logout() {
         loginLink.addEventListener('click', (event) => {
             if (isUserConnected()) {
                 window.sessionStorage.removeItem('token');
-                console.log('Token removed');
                 updateLoginLink();
-                window.location.href = 'index.html'; // Redirect to index.html when logging out
-                console.log('Redirecting to index.html');
             } else {
                 window.location.href = 'login.html'; // Redirect to login.html when clicking on login
                 updateLoginLink();
-                console.log('Redirecting to login.html');
             }
         });
     }
@@ -103,8 +101,8 @@ const modifPortfolioButton = document.querySelector('.btn_portfolio');
 
 function updateButtons() {
   if (isUserConnected()) {
-    modifButton.style.display = 'block';
-    modifPortfolioButton.style.display = 'block';
+    modifButton.style.display = 'flex';
+    modifPortfolioButton.style.display = 'flex';
   } else {
     modifButton.style.display = 'none';
     modifPortfolioButton.style.display = 'none';
@@ -114,6 +112,33 @@ function updateButtons() {
 updateButtons();
 
 
+/* TRYING MODAL */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btnModal = document.getElementById("myBtnModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnModal.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 /*
 
 TO DO : 

d3a79195d945a3e9be7bbf1f36fd84ba68777fd7
axllema <axellexm@gmail.com>
2023-10-24 17:02:52 +0200

    adding a container+element for the trash icon so that later we can delete things in modal. + BIG indentation changes + comments cleaning and adding

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,44 +1,59 @@
-let works = "http://localhost:5678/api/works";
-const gallery = document.getElementById('gallery')
+let works = 'http://localhost:5678/api/works';
+const gallery = document.getElementById('gallery');
 
+// Fetch works data from the API
 async function getWorks() {
-  const response = await fetch(works);
-  const responseWork = await response.json();
-  createGallery(responseWork);
+  try {
+    // Send a GET request to the 'works' API endpoint to fetch the list of works
+    const response = await fetch(works);
+    // Check if the response status is okay (HTTP status 200)
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    // Parse the response body as JSON and store it in 'responseWork'
+    const responseWork = await response.json();
+    // Call the 'createGallery' function and pass the parsed works data to it
+    createGallery(responseWork);
+  } catch (error) {
+    // Handle errors, such as network issues or failed requests
+    console.error('Error fetching data:', error);
+  }
 }
 getWorks();
 
+// Create HTML elements for each work and add them to the gallery
 function createGallery(works) {
   works.forEach((work) => {
-    // creating the HTML elements
+    // Creating the HTML elements
     const figure = document.createElement('figure');
     const image = document.createElement('img');
     const figcaption = document.createElement('figcaption');
 
-    // adding the data to those HTML elements
+    // Adding the data to those HTML elements
     image.src = work.imageUrl;
     image.alt = work.title;
     figcaption.textContent = work.title;
 
     figure.setAttribute('data-category', work.category.id);
-    /* setting the data-category attribute with the category ID - this allows us to associate each image with a specific category for filtering. */
-    console.log("Item category:", work.category);
+    /* Setting the data-category attribute with the category ID - this allows us to associate each image with a specific category for filtering. */
+    console.log('Item category:', work.category);
 
-    // adding img & figcaption to figure
+    // Adding img & figcaption to figure
     figure.appendChild(image);
     figure.appendChild(figcaption);
 
-    // & adding the HTML elements to the gallery
+    // Adding the HTML elements to the gallery
     gallery.appendChild(figure);
   });
 }
 
-function isUserConnected(){
+// Check if the user is connected (has a token in sessionStorage)
+function isUserConnected() {
   const token = window.sessionStorage.getItem('token'); // Store the token in sessionStorage
   if (token) {
-      return true;
+    return true;
   } else {
-      return false;
+    return false;
   }
 }
 
@@ -46,64 +61,57 @@ let loginLink = document.getElementById('loginLink');
 
 function updateLoginLink() {
   if (isUserConnected()) {
-      loginLink.textContent = 'logout';
+    loginLink.textContent = 'logout';
   } else {
-      loginLink.textContent = 'login';
+    loginLink.textContent = 'login';
   }
 }
-
 updateLoginLink();
 
 function logout() {
   loginLink.addEventListener('click', (event) => {
-      if (isUserConnected()) {
-          window.sessionStorage.removeItem('token');
-          updateLoginLink();
-      } else {
-          window.location.href = 'login.html'; // Redirect to login.html when clicking on login
-          updateLoginLink();
-      }
+    if (isUserConnected()) {
+      window.sessionStorage.removeItem('token');
+      updateLoginLink();
+    } else {
+      window.location.href = 'login.html'; // Redirect to login.html when clicking on login
+      updateLoginLink();
+    }
   });
 }
 
-// let categories = "http://localhost:5678/api/categories";
-
 /* FILTERS */
 const filtersArray = [];
-// initializes the variable with an empty array - used to store or manage data related to filters
+// Initializes the variable with an empty array - used to store or manage data related to filters
 
-const filtersContainer = document.querySelector(".filters_div");
-const allFilters = document.querySelector(".btn_filters");
-
-
-let currentIndex = 0;
+const filtersContainer = document.querySelector('.filters_div');
+const allFilters = document.querySelector('.btn_filters');
 
 async function createFilterButtons() {
-  const categoriesResponse = await fetch("http://localhost:5678/api/categories");
+  const categoriesResponse = await fetch('http://localhost:5678/api/categories');
   // Fetch the categories data from the API
   const categoryData = await categoriesResponse.json();
 
   // Create a "Tous" button (All)
-  const allFilterButton = document.createElement("div");
-  allFilterButton.classList.add("btn_filters", "btn_active");
-  allFilterButton.dataset.filter = "all";
-  allFilterButton.textContent = "Tous";
-  // makes the "Tous/all button being in the <div class="filters_div"> - filtersContainer is the parent of allFilterButton
+  const allFilterButton = document.createElement('div');
+  allFilterButton.classList.add('btn_filters', 'btn_active');
+  allFilterButton.dataset.filter = 'all';
+  allFilterButton.textContent = 'Tous';
+  // Makes the "Tous/all" button being in the <div class="filters_div"> - filtersContainer is the parent of allFilterButton
   filtersContainer.appendChild(allFilterButton);
 
+  allFilterButton.addEventListener('click', activatedFilter);
 
-  allFilterButton.addEventListener("click", activatedFilter);
+  categoryData.forEach((category) => {
+    const filterButton = document.createElement('div');
+    filterButton.classList.add('btn_filters');
+    filterButton.dataset.filter = category.id;
+    // Store the category ID as a data attribute
+    filterButton.textContent = category.name;
 
-categoryData.forEach((category) => {
-  const filterButton = document.createElement("div");
-  filterButton.classList.add("btn_filters");
-  filterButton.dataset.filter = category.id;
-  // Store the category ID as a data attribute
-  filterButton.textContent = category.name;
+    filterButton.addEventListener('click', activatedFilter);
 
-  filterButton.addEventListener("click", activatedFilter);
-
-  filtersContainer.appendChild(filterButton);
+    filtersContainer.appendChild(filterButton);
   });
 }
 
@@ -112,54 +120,53 @@ createFilterButtons();
 
 function activatedFilter(event) {
   const selectedFilter = event.target.dataset.filter;
-  console.log("Selected filter:", selectedFilter);
+  console.log('Selected filter:', selectedFilter);
 
-  const filterButtons = document.querySelectorAll(".btn_filters");
+  const filterButtons = document.querySelectorAll('.btn_filters');
 
   // Remove 'btn_active' class from all filter buttons
   filterButtons.forEach((button) => {
-      button.classList.remove("btn_active");
+    button.classList.remove('btn_active');
   });
 
   // Add 'btn_active' class to the clicked filter button
-  event.target.classList.add("btn_active");
+  event.target.classList.add('btn_active');
 
   filterGallery(selectedFilter);
 }
 
 function filterGallery(selectedFilter) {
-  // selects all the figure elements with the class "gallery" and stores them in 'galleryItems'.
-  const galleryItems = document.querySelectorAll(".gallery figure");
+  // Selects all the figure elements with the class "gallery" and stores them in 'galleryItems'.
+  const galleryItems = document.querySelectorAll('.gallery figure');
 
-  // loops through each figure element in the gallery
+  // Loops through each figure element in the gallery
   galleryItems.forEach((item) => {
-    // get the category of the current item from its data attribute
+    // Get the category of the current item from its data attribute
     const itemCategory = item.dataset.category;
     console.log(itemCategory);
-    // Check if the selectedFilter is "all" or matches the item's category
-    if (selectedFilter === "all" || selectedFilter === itemCategory) {
-      item.style.display = "block"; 
+    /* Check if the selectedFilter is "all" or matches the item's category -
+    '===' checks if selectedFilter is exactly equal to "all." If not exactly equal -> moves to the second condition.
+    '||' logical OR operator - allows to combine conditions, checks if 'selectedFilter' is equal to "all" or if it is equal to 'itemCategory'. */
+    if (selectedFilter === 'all' || selectedFilter === itemCategory) {
+      item.style.display = 'block';
     } else {
-      item.style.display = "none"; 
+      item.style.display = 'none';
     }
   });
 }
 
-
-
-// When the page is loaded, direclty shows ALL the works by default
+// When the page is loaded, directly shows ALL the works by default
 filterGallery('all');
 
-
 function updateFilters() {
-    if (isUserConnected()) {
-      filtersContainer.style.display = 'none';
-    } else {
-      filtersContainer.style.display = 'flex';
-      filtersContainer.style.justifyContent = 'center';
-      filtersContainer.style.gap = '10px';
-      filtersContainer.style.marginBottom = '50px';
-    }
+  if (isUserConnected()) {
+    filtersContainer.style.display = 'none';
+  } else {
+    filtersContainer.style.display = 'flex';
+    filtersContainer.style.justifyContent = 'center';
+    filtersContainer.style.gap = '10px';
+    filtersContainer.style.marginBottom = '50px';
+  }
 }
 updateFilters();
 
@@ -176,7 +183,7 @@ function updateButtons() {
 }
 updateButtons();
 
-/* edition mode, black thingy at the top of homepage */
+/* Edition mode, black thingy at the top of homepage */
 
 const editLine = document.querySelector('.edition_mode');
 const body = document.body;
@@ -186,95 +193,92 @@ function showBanner() {
     editLine.style.display = 'flex';
     editLine.style.position = 'fixed';
     body.style.paddingTop = '55px';
-
   } else {
     editLine.style.display = 'none';
   }
 }
 showBanner();
 
-/* works for modal */
+/* Works for modal */
 
 async function getWorksForModal() {
-// Call your function to populate the modal
-  const response = await fetch("http://localhost:5678/api/works");
+  // Call your function to populate the modal
+  const response = await fetch('http://localhost:5678/api/works');
   const data = await response.json();
-  
-  const modalGallery = document.querySelector(".modal-gallery");
-  modalGallery.innerHTML = "";
 
-  // Loop through the data from the API to create elements
-  data.forEach((item, index) => {
-    const workElement = document.createElement("figure");
-    workElement.classList.add("figureModal");
-    // Generate a unique ID for each <figure> element based on the work's ID
-    const dynamicId = item.id;
-    // Create a unique ID by combining "figureModal" and the dynamic ID
-    const concatenedId = "figureModal" + dynamicId;
-    // Set the ID of the <figure> element to the unique ID
-    workElement.id = concatenedId;
-    modalGallery.appendChild(workElement);
+  const modalGallery = document.querySelector('.modal-gallery');
+  modalGallery.innerHTML = '';
 
-    const imgWorkElement = document.createElement("img");
-    imgWorkElement.src = item.imageUrl;
-    imgWorkElement.classList.add("imgModal");
-    workElement.appendChild(imgWorkElement);
+// Loop through the data from the API to create elements
+data.forEach((item, index) => {
+  const workElement = document.createElement('figure');
+  workElement.classList.add('figureModal');
+  // Generate a unique ID for each <figure> element based on the work's ID
+  const dynamicId = item.id;
+  // Create a unique ID by combining "figureModal" and the dynamic ID
+  const concatenedId = 'figureModal' + dynamicId;
+  // Set the ID of the <figure> element to the unique ID
+  workElement.id = concatenedId;
+  modalGallery.appendChild(workElement);
 
-    /* Create a container for other actions (delete, add, ...)
-    const trashContainer = document.createElement("div");
-    trashContainer.classList.add("figureContainer", "trashContainer");
-    workElement.appendChild(trashContainer); */
+  const imgWorkElement = document.createElement('img');
+  imgWorkElement.src = item.imageUrl;
+  imgWorkElement.classList.add('imgModal');
+  workElement.appendChild(imgWorkElement);
 
-    /* // Create an icon for deleting the work - later on
-    const trashElement = document.createElement("i");
-    trashElement.classList.add("fa-solid", "fa-trash-can");
-    //trashContainer.appendChild(trashElement);*/
-  }); 
+  // Create a container for other actions (delete, add, ...)
+  const trashContainer = document.createElement('div');
+  trashContainer.classList.add('figureContainer', 'trashContainer');
+  workElement.appendChild(trashContainer);
+
+  // Create an icon for deleting the work - later on
+  const trashElement = document.createElement('i');
+  trashElement.classList.add('fa-solid', 'fa-trash-can');
+  trashContainer.appendChild(trashElement);
+}); 
 }
 
 /* MODAL */
 
-const modal = document.getElementById("myModal");
-const modalContent = document.querySelector(".modal-content");
+const modal = document.getElementById('myModal');
+const modalContent = document.querySelector('.modal-content');
 
-const modalGallery = document.createElement("div");
-modalGallery.classList.add("modal-gallery");
+const modalGallery = document.createElement('div');
+modalGallery.classList.add('modal-gallery');
 // 'modalGallery' is the "child" of 'modal', meaning it's displayed inside the 'modal', on the webpage
-modal.appendChild(modalGallery)
+modal.appendChild(modalGallery);
 
-const btnModal = document.getElementById("btn_open_modal");
+const btnModal = document.getElementById('btn_open_modal');
 // Get the <span> element that closes the modal - [0] is used to get the first element with the class "close."
-const span = document.getElementsByClassName("close")[0];
-
+const span = document.getElementsByClassName('close')[0];
 
 btnModal.onclick = function() {
-  modal.style.display = "flex";
+  modal.style.display = 'flex';
   getWorksForModal();
-}
+};
 
 span.onclick = function() {
-  modal.style.display = "none";
-}
+  modal.style.display = 'none';
+};
 
-const addPhotoBtn = document.createElement("div");
-  addPhotoBtn.classList.add("addPhotoBtn");
-  modal.appendChild(addPhotoBtn);
+const addPhotoBtn = document.createElement('div');
+addPhotoBtn.classList.add('addPhotoBtn');
+modal.appendChild(addPhotoBtn);
 
-  const addPhotoBtnText = document.createElement("p");
-  addPhotoBtnText.innerText = "Ajouter une photo";
-  addPhotoBtn.appendChild(addPhotoBtnText);
+const addPhotoBtnText = document.createElement('p');
+addPhotoBtnText.innerText = 'Ajouter une photo';
+addPhotoBtn.appendChild(addPhotoBtnText);
 
-  const hrLine = document.createElement("hr");
-  modal.appendChild(hrLine);
+const hrLine = document.createElement('hr');
+modal.appendChild(hrLine);
 
 
 /*
 TO DO : 
-- shows gallery in modale
-- put the little grey line (hr) at the right place
 - put the add photo btn at the right place
 - add / delete pictures in the modale !
 - 
 
+- put the little grey line (hr) at the right place
 why is the console showing more & more things? ask mentor
 */
--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -2,78 +2,62 @@ const loginForm = document.getElementById('login_form');
 let loginLink = document.getElementById('loginLink');
 
 function redirectHomepage() {
-  // returns the href (URL) of the current page - https://www.scaler.com/topics/javascript-window-location/
-  window.location.href = 'index.html';
+    window.location.href = 'index.html';
 }
 
 loginForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const user = {
+        email: email.value,
+        password: password.value
+    };
 
-  //prevent refreshing the whole page every time the button is clicked
-  event.preventDefault();
+    await fetch('http://localhost:5678/api/users/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        if (result && result.token) {
+            alert('Bienvenue!');
+            window.sessionStorage.setItem('token', result.token);
+            updateLoginLink();
+            redirectHomepage();
+        } else {
+            alert('Identifiant ou mot de passe incorrect.');
+        }
+    });
+});
 
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  const user = {
-    email: email.value,
-    password: password.value
-  };
-
-await fetch('http://localhost:5678/api/users/login', {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  /* assigning an object with keys that represent the information the server needs
-  JSON.stringify() means we are sending the information in a string form to the server */
-  body: JSON.stringify(user),
-  })
-  .then((response) => response.json())
-  .then((result) => {
-    if (result && result.token) {
-      // If result is truthy and result.token is truthy, we're logged in
-      alert('Bienvenue!');
-      window.sessionStorage.setItem('token', result.token); // Store the token in sessionStorage
-      updateLoginLink(); // Update login link text
-    // Redirect to the homepage
-    redirectHomepage();
+function isUserConnected() {
+    const token = window.sessionStorage.getItem('token');
+    if (token) {
+        return true;
     } else {
-      alert('Identifiant ou mot de passe incorrect.');
+        return false;
     }
-  });
-})
-
-function isUserConnected(){
-  const token = window.sessionStorage.getItem('token'); // Store the token in sessionStorage
-  console.log('token', token);
-  if (token) {
-      return true;
-  } else {
-      return false;
-  }
 }
+
 function updateLoginLink() {
-  if (isUserConnected()) {
-      loginLink.textContent = 'logout';
-      loginLink.style.fontSize = '1.2em';
-  } else {
-      loginLink.textContent = 'login';
-      loginLink.style.fontSize = '1.2em';
-  }
+    if (isUserConnected()) {
+        loginLink.textContent = 'logout';
+        loginLink.style.fontSize = '1.2em';
+    } else {
+        loginLink.textContent = 'login';
+        loginLink.style.fontSize = '1.2em';
+    }
 }
 
 function logout() {
-  loginLink.addEventListener('click', (event) => {
-    if (isUserConnected()) {
-      window.sessionStorage.removeItem('token');
-      updateLoginLink();
-  }
-});
+    loginLink.addEventListener('click', (event) => {
+        if (isUserConnected()) {
+            window.sessionStorage.removeItem('token');
+            updateLoginLink();
+        }
+    });
 }
-
-/*
-TO DO : 
-- have an error message when email not right
-- have another error message when password not right
-
-- try to have a modal for the forgotten password?
-*/
